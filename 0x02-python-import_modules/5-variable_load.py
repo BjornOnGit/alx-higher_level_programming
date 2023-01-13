6791821f899116f7257f69e7b6eb39ff38df37c0
Francis <fhrancorey99@gmail.com>
2023-01-13 02:10:28 +0100

    imports we have brought

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+# Eze Francis Ogonnaya
+
+"""Print the value of variable a from variable_load_5."""
+if __name__ == "__main__":
+    from variable_load_5 import a
+
+    print(a)
