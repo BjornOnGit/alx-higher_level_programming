e21c16e5b4b35697c647d0748cb689abd916982b
EzeFrancis <99059099+BjornOnGit@users.noreply.github.com>
2023-05-03 21:45:45 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    squared = []
-    for i in matrix:
-        squared.append(list(map(lambda x: x ** 2, i)))
-    return squared
--- 0x04-python-more_data_structures/README.md
+++
@@ -1,1 +0,0 @@
-# This folder contains tasks on Sets and Dictionary
