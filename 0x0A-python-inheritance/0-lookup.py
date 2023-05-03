6b9d9709ec71bbe139afdd42e5124fd60ef7b88b
EzeFrancis <99059099+BjornOnGit@users.noreply.github.com>
2023-05-03 21:49:30 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" This module looks up objects"""
-
-
-def lookup(object):
-    """This function looks up properties using
-    ``dir``"""
-    return list(dir(object))
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This module creates a class called ``BaseGeometry``"""
-
-
-class BaseGeometry:
-    """Creation of BaseGeometry class"""
-    pass
