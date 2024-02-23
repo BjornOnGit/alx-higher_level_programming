b6c2dcba8f363ba1b5af368c3db7bc2c8b0cc1ae
Francis <fhrancorey99@gmail.com>
2024-02-24 00:35:04 +0100

    fix: advanced tasks

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""
-Deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa.
-Usage: ./100-relationship_states_cities.py <mysql username> /
-                                    <mysql password> /
-                                    <database name>
-"""
-from sys import argv
+# Creates the State “California” with the City “San Francisco”
+# from the database hbtn_0e_100_usa.
+# Usage: ./100-relationship_states_cities.py <mysql username> /
+#                                            <mysql password> /
+#                                            <database name>
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
+from relationship_state import State
+from relationship_city import Base, City
 
-
-def relationship():
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(user, passwd, db), pool_pre_ping=True)
-
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    # To start a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = State(name="California")
-    city = City(name="San Francisco")
-
-    state.cities.append(city)
-    session.add(state)
+    session.add(City(name="San Francisco", state=State(name="California")))
     session.commit()
-    session.close()
-
-
-if __name__ == "__main__":
-    relationship()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-Defines a State model.
-Inherits from SQLAlchemy Base and links to the MySQL table states.
-"""
-
+# Defines a State model.
+# Inherits from SQLAlchemy Base and links to the MySQL table states.
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
 
 
 class State(Base):
-    """ Model State """
+    """Represents a state for a MySQL database.
 
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        name (sqlalchemy.String): The state's name.
+        cities (sqlalchemy.orm.relationship): The State-City relationship.
+    """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
-                unique=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          cascade="all, delete-orphan", backref="state")
+
+    cities = relationship("City", backref="state", cascade="all, delete")
